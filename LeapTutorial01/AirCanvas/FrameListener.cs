3e27c0b205e6ae21b6b71200cd4cfb75810ae832
Keiho Sakapon <git@saka-pon.net>
2014-10-25 00:31:40 +0900

    Add FrameListener class

---
+++ LeapTutorial01/AirCanvas/FrameListener.cs
@@ -0,0 +1,22 @@
+﻿using Leap;
+using System;
+
+namespace AirCanvas
+{
+    class FrameListener : Listener
+    {
+        public event Action<Frame> FrameArrived;
+
+        public override void OnFrame(Controller controller)
+        {
+            var h = FrameArrived;
+            if (h != null)
+            {
+                using (var frame = controller.Frame())
+                {
+                    h(frame);
+                }
+            }
+        }
+    }
+}
